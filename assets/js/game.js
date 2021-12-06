d88b1f135bc8be0a32c469a58770bc96ff6edc66
Brandy <brandyraquel323@gmail.com>
2021-12-06 12:20:41 -0500

    updated js

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@
 var playerName = window.prompt("What is your robot's name?");
 
 // Note the lack of quotation marks around playerName
-console.log();(playerName);
+console.log(playerName);
 
 function fight() {
     window.alert("The fight has begun!");
