3c733b601597623a60da198d4823015c132e860c
Brandy <brandyraquel323@gmail.com>
2021-12-06 12:15:18 -0500

    added var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 // this creates a function named "fight"
 
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quotation marks around playerName
+console.log();(playerName);
+
 function fight() {
     window.alert("The fight has begun!");
   }
+
+  fight ();
